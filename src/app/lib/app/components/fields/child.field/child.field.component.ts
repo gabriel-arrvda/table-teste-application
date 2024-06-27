0747d3ddae77f3b686d6555f898653f23d305ce4
Gabriel Arruda <gabriellucas_arruda@hotmail.com>
2024-06-27 14:22:46 -0300

    update to 18

--- src/app/lib/app/components/fields/child.field/child.field.component.ts
+++ src/app/lib/app/components/fields/child.field/child.field.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { ChildField } from 'src/app/lib/models/fields/fields';
 
 @Component({
   selector: 'app-child.field',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './child.field.component.html',
   styleUrls: ['./child.field.component.scss']
 })
--- src/app/lib/app/components/fields/simple.field/simple.field.component.ts
+++ src/app/lib/app/components/fields/simple.field/simple.field.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { SimpleText } from 'src/app/lib/models/fields/fields';
 
 @Component({
   selector: 'app-simple.field',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './simple.field.component.html',
   styleUrls: ['./simple.field.component.scss']
 })
